3802258a1031789bddc23662d8c96a232fe18a0a
stevebennn <sbennett@oyosportstoys.com>
2016-07-05 12:14:53 -0400

    some other dumb stuff

--- frequentJS.js
+++ frequentJS.js
@@ -1,76 +1,42 @@
-// returns the specified number of characters starting from the left.
-// String.prototype.left = function(num) { return this.slice(0,num) };
-
-// returns the specified number of characters starting from the right.
-// String.prototype.right = function(num) { return this.slice(this.length-num) };
-
-// returns the specified number of characters between the the starting and ending index.
-// String.prototype.mid = function(sNum,eNum) { return this.substring((eNum+sNum-1),(sNum-1)) };
-
-// returns the string in lowercase.
-// String.prototype.lower = function() { return this.toLowerCase() };
-
-// returns the string in uppercase.
-String.prototype.upper = function() { return this.toUpperCase() };
-
 // returns the specified number of characters in uppercase starting from either the left,right or middle.
-String.prototype.upperFrom = function(dir,num,num2) {
-    switch (dir){
-        case 'left': return this.left(num).upper() + this.right(this.length-num);
-            break;
-        case 'mid' : return this.left(num-1) + this.mid(num,num2).upper() + this.right(this.length-num2-1);
-            break;
-        case 'right' : return this.left(this.length-num) + this.right(num).upper();
-            break;
-    }
-};
+// String.prototype.upperFrom = function(dir,num,num2) {
+//     switch (dir){
+//         case 'left': return this.left(num).upper() + this.right(this.length-num);
+//             break;
+//         case 'mid' : return this.left(num-1) + this.mid(num,num2).upper() + this.right(this.length-num2-1);
+//             break;
+//         case 'right' : return this.left(this.length-num) + this.right(num).upper();
+//             break;
+//     }
+// };
 
-// returns true or false if the string exists in the array passed.
-String.prototype.exists = function(arr) { return arr.indexOf(this) != -1 };
-
-// returns the string truncated after character limit is met.
-String.prototype.truncate = function(num){
-    return this.length > num ? this.left(num) + "..." : this;
-};
+// // returns the string truncated after character limit is met.
+// String.prototype.truncate = function(num){
+//     return this.length > num ? this.left(num) + "..." : this;
+// };
 
 // returns an array using spaces as the delimiter.
-String.prototype.toArray = function() {
-    return this.split(" ");
-};
+// String.prototype.toArray = function() {
+//     return this.split(" ");
+// };
 
 // returns the value of a key in a array of object literals.
-String.prototype.lookup = function(arr) {
-    let self = this;
-    return arr.find(function(arr){
-        return arr[self]
-    })[self]
-};
+// String.prototype.lookup = function(arr) {
+//     let self = this;
+//     return arr.find(function(arr){
+//         return arr[self]
+//     })[self]
+// };
 
 // returns the word specified by index ( does not use a 0 index )
-String.prototype.word = function(num) { return this.split(" ")[num-1]; };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// String.prototype.word = function(num) { return this.split(" ")[num-1]; };
 
 
 
 let fq = (function() {
 
     let methods =  {};
-    let _arg = "";
-
-    methods.val = _arg;
+    methods.val = "";
 
     // snagging the value we want to manipulate
     methods.request = function(arg) {
@@ -78,31 +44,50 @@ let fq = (function() {
         return this;
     };
 
-    //
+    // returns the specified number of characters starting from the left.
     methods.left = function(num) {
         this.val = this.val.slice(0,num);
         return this
-
     };
 
+    // returns the specified number of characters starting from the right.
     methods.right = function(num) {
-        this.val = this.val.slice(0,num);
+        this.val = this.val.slice(this.val.length-num);
         return this
-
     };
 
+    // returns the specified number of characters between the the starting and ending index.
     methods.mid = function(sNum,eNum) {
         this.val = this.val.substring((eNum+sNum-1),(sNum-1));
         return this
-    }
+    };
 
+    // returns the string in lowercase.
+    methods.lower = function() {
+        this.val = this.val.toLowerCase();
+        return this
+    };
+
+    // returns the string in uppercase.
+    methods.upper = function() {
+        this.val = this.val.toUpperCase();
+        return this
+    };
+
+    // returns true or false if the string exists in the array passed.
+    methods.exists = function(arr) {
+        this.val = arr.indexOf(this.val) != -1;
+        return this
+    };
+
+    // returns the string value of the val property
     methods.val = function() {
         return this.val;
-    }
+    };
 
     return methods;
 
 })();
 
-
-console.log(fq.request('asdf').mid(2,2).left(1).val);
+let arr = ['this','that', 'the other thing'];
+console.log(fq.request('this').exists(arr).val);
