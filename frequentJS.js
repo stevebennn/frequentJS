7a79daec050ca5257c336a028d08dd9a4c672abb
stevebennn <sbennett@oyosportstoys.com>
2016-07-06 17:55:33 -0400

    updated to be less shitty

--- frequentJS.js
+++ frequentJS.js
@@ -1,93 +1,70 @@
-// returns the specified number of characters in uppercase starting from either the left,right or middle.
-// String.prototype.upperFrom = function(dir,num,num2) {
-//     switch (dir){
-//         case 'left': return this.left(num).upper() + this.right(this.length-num);
-//             break;
-//         case 'mid' : return this.left(num-1) + this.mid(num,num2).upper() + this.right(this.length-num2-1);
-//             break;
-//         case 'right' : return this.left(this.length-num) + this.right(num).upper();
-//             break;
-//     }
-// };
 
-// // returns the string truncated after character limit is met.
-// String.prototype.truncate = function(num){
-//     return this.length > num ? this.left(num) + "..." : this;
-// };
+let fq = function() {
 
-// returns an array using spaces as the delimiter.
-// String.prototype.toArray = function() {
-//     return this.split(" ");
-// };
+    this.val = "";
 
-// returns the value of a key in a array of object literals.
-// String.prototype.lookup = function(arr) {
-//     let self = this;
-//     return arr.find(function(arr){
-//         return arr[self]
-//     })[self]
-// };
-
-// returns the word specified by index ( does not use a 0 index )
-// String.prototype.word = function(num) { return this.split(" ")[num-1]; };
-
-
-
-let fq = (function() {
-
-    let methods =  {};
-    methods.val = "";
-
-    // snagging the value we want to manipulate
-    methods.request = function(arg) {
-        this.val = arg;
+    // sucking in the value we want to work with
+    this.rq = function(val) {
+        this.val = val;
         return this;
     };
 
     // returns the specified number of characters starting from the left.
-    methods.left = function(num) {
-        this.val = this.val.slice(0,num);
-        return this
+    this.left = function(num) {
+        this.val = this.val.substr(0,num);
+        return this;
     };
 
     // returns the specified number of characters starting from the right.
-    methods.right = function(num) {
-        this.val = this.val.slice(this.val.length-num);
-        return this
-    };
-
-    // returns the specified number of characters between the the starting and ending index.
-    methods.mid = function(sNum,eNum) {
-        this.val = this.val.substring((eNum+sNum-1),(sNum-1));
-        return this
+    this.right = function(num) {
+        this.val = this.val.slice(-num);
+        return this;
     };
 
     // returns the string in lowercase.
-    methods.lower = function() {
+    this.lower = function() {
         this.val = this.val.toLowerCase();
-        return this
+        return this;
+    };
+
+    // returns the specified number of characters between the the starting and ending index.
+    this.mid = function(sNum,eNum) {
+        this.val = this.val.substring((eNum+sNum-1),(sNum-1));
+        return this;
     };
 
     // returns the string in uppercase.
-    methods.upper = function() {
+    this.upper = function() {
         this.val = this.val.toUpperCase();
-        return this
+        return this;
     };
 
     // returns true or false if the string exists in the array passed.
-    methods.exists = function(arr) {
+    this.exists = function(arr) {
         this.val = arr.indexOf(this.val) != -1;
-        return this
+        return this;
     };
 
-    // returns the string value of the val property
-    methods.val = function() {
-        return this.val;
+    //returns an array using spaces as the delimiter.
+    this.toArray = function() {
+        this.val = this.val.split(" ");
+        return this;
     };
 
-    return methods;
+    // returns the string truncated after character limit is met.
+    this.truncate = function(num){
+        this.val.length > num ? this.val = this.val.substr(0,num) + "..." : this.val = this.val;
+        return this;
+    };
 
-})();
+    //returns the word specified by index ( does not use a 0 index )
+    this.word = function(num) {
+        this.val = this.val.split(" ")[num-1];
+        return this;
+    };
 
-let arr = ['this','that', 'the other thing'];
-console.log(fq.request('this').exists(arr).val);
+};
+
+String.prototype.fq = function() {
+    return new fq().rq(this);
+};
